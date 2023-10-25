05f279475e8080393987ccf292a1923f232e3553
codingpaige <paige_ng@outlook.com>
2023-10-25 15:44:35 +0800

    Added weather icons from Openweather API & edited timezone to reflect city's timezone.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-function formatDayandDate(timestamp) {
+function formatDate(timestamp) {
   let date = new Date(timestamp);
   let days = [
     "Sunday",
@@ -23,21 +23,28 @@ function formatDayandDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let cityName = document.querySelector(".city-name");
   let cityTemp = document.querySelector(".temp-value");
   let weatherDescription = document.querySelector(".weather-description");
   let humidity = document.querySelector(".humidity");
   let wind = document.querySelector(".wind");
   let dayAndTime = document.querySelector(".day-and-time");
+  let weatherIcon = document.querySelector("#weather-icon");
 
   cityName.innerHTML = response.data.name;
   cityTemp.innerHTML = Math.round(response.data.main.temp);
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = Math.round(response.data.main.humidity);
   wind.innerHTML = Math.round(response.data.wind.speed);
-  dayAndTime.innerHTML = formatDayandDate(response.data.dt * 1000);
+  dayAndTime.innerHTML = formatDate(response.data.timezone * 1000);
+  weatherIcon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", `${response.data.weather[0].description}`);
 }
 
 let apiKey = "294ff1c8d42a84a37badb92d65cbfb69";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=singapore&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
